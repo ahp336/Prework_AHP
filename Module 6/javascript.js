4747917a431653d22bb7c0d13243533c5dcc2909
ahp336 <aakashparikh8@gmail>
2019-07-28 00:00:12 -0400

    I just upload my pre-work on git-hub

---
+++ Module 6/javascript.js
@@ -0,0 +1,36 @@
+    document.getElementById("button1").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "400px";
+
+});
+
+    document.getElementById("button2").addEventListener("click", function(){
+
+    document.getElementById("box").style.background = "blue";
+
+});
+
+    document.getElementById("button3").addEventListener("click", function(){
+
+    document.getElementById("box").style.display = "none";
+
+});
+
+    document.getElementById("button4").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "150px";
+
+
+});
+
+    document.getElementById("button4").addEventListener("click", function(){
+
+    document.getElementById("box").style.background = "orange";
+
+});
+
+    document.getElementById("button4").addEventListener("click", function(){
+
+    document.getElementById("box").style.display = "block";
+
+});
